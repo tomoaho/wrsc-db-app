4d289bf206f95862a924d67a3cf4c925d760b62a
tomoaho <tomoharusuda@MacBook-Air-3.local>
2025-12-12 21:42:17 +0900

    Fix db creation for production

--- main.py
+++ main.py
@@ -605,6 +605,8 @@ def download_db():
     # 3. どちらにもない場合
     return "エラー: データベースファイルが見つかりませんでした。(shooting.db)", 404
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context(): db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5001)
